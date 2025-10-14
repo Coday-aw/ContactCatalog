af1b24221549157c2ff6c8115d8a628dad5fa768
coday-aw <coday-aw@hotmail.com>
2025-10-14 21:52:12 +0200

    project start

---
+++ ContactCatalog1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ContactCatalog1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        while (true)
+        {
+            Console.WriteLine("==== Contact Catalog ====");
+            Console.WriteLine("\n1)Add new contact\n2)List contacts\n3)Search by name\n4)Filter by tag\n5)Export CSV\n0)Exit");
+            string? choose = Console.ReadLine();
+            break;
+
+        }
+    }
+}
